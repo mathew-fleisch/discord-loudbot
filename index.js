8835639ec309aa7a2f9e5b3f65048c14e7a9d7e0
mathew-fleisch <mathew.fleisch@gmail.com>
2021-03-18 09:37:01 -0700

    Remove semicolons and add deduplication

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const client = new Discord.Client();
+require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client()
 
-let dblite = require('dblite');
-let db = dblite(process.env.PATH_TO_SQLITE_DB);
+let dblite = require('dblite')
+let db = dblite(process.env.PATH_TO_SQLITE_DB)
 
-db.query("CREATE TABLE IF NOT EXISTS loudbot (yells TEXT)");
+db.query("CREATE TABLE IF NOT EXISTS loudbot (yells TEXT)")
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.channel.id == process.env.RESTRICT_CHANNEL && msg.author.id != process.env.EXCLUDE_BOT) {
-    if (!msg.content.match(/[a-z]/)) {
-      // TODO: exclude duplicates
-      db.query("INSERT INTO loudbot (yells) VALUES (?)", [msg.content]);
-      db.query("SELECT * FROM loudbot ORDER BY RANDOM() LIMIT 1", function(err, res) {
-         if (err) {
-           console.log("ERROR:",err);
-           return;
-         }
-         msg.channel.send(res);
-      });
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+async function getRandomYell(db, msg) {
+  console.log("getRandomYell()")
+  db.query("SELECT * FROM loudbot ORDER BY RANDOM() LIMIT 1", async function(err, res) {
+    if (err) {
+      console.log("ERROR: ",err)
+      return
     }
+    await msg.channel.send(res)
+  })
+}
+client.on('message', async msg => {
+  // console.log(msg)
+  if (msg.channel.id != process.env.RESTRICT_CHANNEL) {
+    console.log("Do not display in this channel: " + msg.channel.id)
+    return
   }
-});
-client.login(process.env.DISCORD_TOKEN);
-
-
+  if (msg.author.id == process.env.EXCLUDE_BOT) {
+    console.log("Do not react to bot messages: " + msg.author.id + " " + process.env.EXCLUDE_BOT)
+    return
+  }
+  // console.log("Not a bot message and in the restricted channel")
+  if (!msg.content.match(/[a-z]/)) {
+    db.query("SELECT * FROM loudbot WHERE yells = ?", [msg.content], async function(err, res) {
+      if (err) {
+        console.log("ERROR: ",err)
+        return
+      }
+      await getRandomYell(db, msg)
+      if (res.length == 0) {
+        db.query("INSERT INTO loudbot (yells) VALUES (?)", [msg.content])
+        return
+      }
+    })
+  }
+})
+client.login(process.env.DISCORD_TOKEN)
